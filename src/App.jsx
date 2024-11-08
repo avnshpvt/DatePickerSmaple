28f33b6721156f586d72ecbe7d041c6477301edb
avnshpvt <avnshpvt@gmail.com>
2024-11-08 20:56:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResponsiveDatePickers from './DatePickersExample';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveDatePickers />
+    </div>
+  );
+}
+
+export default App;
+
